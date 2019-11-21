717c49a085fce28846b0d594cfd66f4891f82ec8
cperez21-at-898747083820 <cullen.perez64@gmail.com>
2019-11-20 23:43:42 -0500

    Work in progress on ragdoll characters. Temporarily removed HurtTransform until set up is finished.

---
+++ Chernobyl Chickens/Assets/Scripts/PrintTransform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintTransform : MonoBehaviour
+{
+    Transform myTransform;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject.name + "Transform position is " + transform.position);
+        if(gameObject.GetComponent<SkinnedMeshRenderer>())
+        {
+           SkinnedMeshRenderer smr = gameObject.GetComponent<SkinnedMeshRenderer>();
+            //Debug.Log(gameObject.name + "Transform position is " + smr.ce)
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
