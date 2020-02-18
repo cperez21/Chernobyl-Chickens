f1a2ed2a129137e03accd51c6dd9833afefafb22
cperez21-at-898747083820 <cullen.perez64@gmail.com>
2020-02-17 22:06:08 -0500

    Character will now get up at the location where they currently are when fallen, not warp back to their original standing position.

--- Chernobyl Chickens/Assets/Scripts/PlayerController.cs
+++ Chernobyl Chickens/Assets/Scripts/PlayerController.cs
@@ -13,19 +13,21 @@ public class PlayerController : MonoBehaviour
     private float timer;
     private float dirX;
     private float dirZ;
+    public float stunAmount;
 
     // [Header("Audio Section")]
 
     public AudioClip slap1, slap2, slap3, slap4, slap5;
     private AudioSource audioS;
-
-
+    [Tooltip("this should be bip01 Pelvis under the Non-Puppet master")]
+    public GameObject getUpPosition;
     public bool canJump = false;
     Vector3 moveInput;
     Vector3 startOrientation;
     public float moveSpeed, jumpForce;
     public int health = 100; //Health set to be between 0.0 and 1.0 because of puppet master settings. -cullen
     private float healthF = 1.0f;
+   
     public bool canHurt;
     public bool isPlayer2; //currently used for 2 player only prototype
     public bool haveControls; //Currently used for testing
@@ -33,6 +35,8 @@ public class PlayerController : MonoBehaviour
     Vector3 respawnPoint, moveVelocity;
     Animator anim;
     private float animTimer = 0.0f;
+    public float defaultAnimSpeed, defaultMoveSpeed;
+   
     RaycastHit hit; //Used to check for jumps
 
 
@@ -46,7 +50,7 @@ public class PlayerController : MonoBehaviour
     public float strikeForce;
     public int strikeDamage;
     private float attackBonus = 0f;
-    private float attackTimer = 0f;
+   
 
 
     //used to declare controls
@@ -55,25 +59,30 @@ public class PlayerController : MonoBehaviour
     private string JumpControl;
     private string StrikeControl;
 
-    IEnumerator Attacking()
-    {
-        attackBonus = 2f;
-        yield return new WaitForSeconds(1.0f);
-        attackBonus = 0f;
-    }
+    
 
     IEnumerator Stunned()
     {
+        puppet.state = RootMotion.Dynamics.PuppetMaster.State.Dead;
         puppet.pinDistanceFalloff += 30f;
+        moveSpeed *= stunAmount;
+        anim.speed *= stunAmount;
         Debug.Log("Stunned");
         haveControls = false;
-        //puppet.state = RootMotion.Dynamics.PuppetMaster.State.Dead;
 
-        yield return new WaitForSeconds(0.5f);
+       
 
-        //puppet.state = RootMotion.Dynamics.PuppetMaster.State.Alive;
-
+        yield return new WaitForSeconds(2.5f);
+        
+        puppet.state = RootMotion.Dynamics.PuppetMaster.State.Alive;
+        puppet.targetRoot.position = getUpPosition.transform.position;
         haveControls = true;
+        moveSpeed *= stunAmount;
+        anim.speed *= stunAmount;
+
+        //Vector3 y = Vector3.Project(puppet.targetRoot.position - rb.position, puppet.targetRoot.up);
+        //puppet.targetRoot.position += y;
+       
     }
 
     public enum PlayerState
@@ -98,14 +107,15 @@ public class PlayerController : MonoBehaviour
     {
         startOrientation = transform.rotation.eulerAngles;
         audioS = GetComponent<AudioSource>();
-
+       
         state = PlayerState.DEFAULT;
         rb = gameObject.GetComponent<Rigidbody>();
         puppet = transform.parent.GetChild(1).GetComponent<RootMotion.Dynamics.PuppetMaster>(); //Good god
         limbs = transform.parent.GetChild(1).GetComponentsInChildren<LimbDamage>();
         //rbPuppet = transform.parent.GetChild(1).GetComponentsInChildren<Rigidbody>(); //rbPuppet[7] is the head.
         anim = GetComponent<Animator>();
-
+        defaultAnimSpeed = anim.speed;
+        defaultMoveSpeed = moveSpeed;
 
         respawnPoint = transform.position;
         strikeForce = 1;
@@ -143,18 +153,13 @@ public class PlayerController : MonoBehaviour
         timer += Time.deltaTime;
 
         DamageCheck();
+        //Recover();
         //puppet.pinWeight = healthF;
 
-        //Stun recovery. Snaps the character back into shape over time.
-        if (puppet.pinDistanceFalloff >= 5f)
+        if(Input.GetKeyDown(KeyCode.T))
         {
-            puppet.pinDistanceFalloff -= 0.5f;
+            puppet.targetRoot.position = getUpPosition.transform.position;
         }
-        else
-        {
-            puppet.pinDistanceFalloff = 5f;
-        }
-
 
 
 
@@ -350,7 +355,32 @@ public class PlayerController : MonoBehaviour
 
     }
 
-   
+    void Recover()
+    {
+        //Stun recovery. Snaps the character back into shape over time.
+        if (puppet.pinDistanceFalloff >= 5f)
+        {
+            puppet.pinDistanceFalloff -= 0.5f;
+        }
+        else
+        {
+            puppet.pinDistanceFalloff = 5f;
+        }
+
+
+        if(moveSpeed < defaultMoveSpeed) //movement speed recovery
+        {
+            moveSpeed = 1f;
+            // moveSpeed = (moveSpeed * stunAmount) + moveSpeed;
+        }
+
+        if(anim.speed < defaultAnimSpeed) //movement speed recovery
+        {
+            moveSpeed = 1f;
+            //anim.speed = (anim.speed * stunAmount) + anim.speed;
+        }
+
+    }
 
     //Checks all limbs for any damage
     void DamageCheck()
