8aa5060edd9fe320fa3e3d3c3e06ebd42b00b471
Ceasar <ceasarhowe@hotmail.com>
2020-02-17 19:07:27 -0500

    Updated UI
    Starting from base, added Character Selector

---
+++ Chernobyl Chickens/Assets/Scripts/CharacterSelectorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class CharacterSelectorScript : MonoBehaviour
+{
+    public List<Character> characters = new List<Character>();
+    public GameObject charCellPrefab;
+    
+    void Start()
+    {
+        foreach(Character character in characters)
+        {
+            SpawnCharacterCell(character);
+            Debug.Log(character);
+        }
+    }
+
+    private void SpawnCharacterCell(Character character)
+    {
+        GameObject charCell = Instantiate(charCellPrefab, transform);
+        charCell.name = character.characterName;
+        Text name = charCell.transform.GetComponentInChildren<Text>();
+        Image icon = charCell.transform.GetComponent<Image>();
+
+        icon.sprite = character.characterSprite;
+        name.text = character.characterName;
+    }
+    
+}
