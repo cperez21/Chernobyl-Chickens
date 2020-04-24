5aea98acf1f8101a3d542e338d994ea2597b0c7e
cperez21-at-898747083820 <cullen.perez64@gmail.com>
2020-04-23 20:44:47 -0400

    -Added sprint particle effect trail to all human character prefabs -worked on playerInputSettings to combine mouse and keybaord (not complete yet)

--- Chernobyl Chickens/Assets/Scripts/UIScript/PlayerManagerScript.cs
+++ Chernobyl Chickens/Assets/Scripts/UIScript/PlayerManagerScript.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class PlayerManagerScript : MonoBehaviour
 {
+
+    PlayerInputManager inputManager;
+    PlayerInput[] pInput;
+    InputUser iUser;
+    int numOfUsers;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+      //  inputManager = GetComponent<PlayerInputManager>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(pInput != null)
+        {
+
+        }
+       // pInput = GameObject.FindObjectsOfType<PlayerInput>();
+       
         
+            
+            
     }
 }
