972e9e4b2968b598cf5b1df9e4b698b005eeca05
Ceasar <ceasarhowe@hotmail.com>
2020-05-02 01:20:33 -0400

    made maps playable, fixed sceneindexes

--- Chernobyl Chickens/Assets/Scripts/UIScript/SceneIndexes.cs
+++ Chernobyl Chickens/Assets/Scripts/UIScript/SceneIndexes.cs
@@ -6,11 +6,14 @@ public enum SceneIndexes
 {
   Persistent = 0,
   MenuScene = 1,
-  CharSelect = 2,
-  MapSelect = 3,
-  ChernobylWhiteBox = 4,
-  Oslo = 5,
-  Town = 6,
-  Yucca = 7,
+  //CharSelect = 2,
+  MapSelect = 2,
+  ChernobylWhiteBox = 3,
+  Oslo = 4,
+  Town = 5,
+  Junkyard = 6,
+  CloudArena = 7,
+  Missile = 8,
+  Sarcophogus = 9,
 
 }
